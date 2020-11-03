0b440d57db266e23c261b92cd3eb122335fe0a85
HagiAyato <h8a9g11i.@gmail.com>
2020-11-03 21:17:21 +0900

    list,dataframeの処理速度検証PG追加

---
+++ use10000dataDF.py
@@ -0,0 +1,36 @@
+﻿import pandas as pd
+import time
+
+df = pd.read_csv('10000data.csv', index_col=0)
+
+# 平均
+start = time.time()
+for i in range(10):
+	df['average'] = (df['subjectA'] + df['subjectB'] + df['subjectC'] + df['subjectD'] + df['subjectE'])/5
+elapsed_time = time.time() - start
+print ("平均処理10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
+
+# ソート
+start = time.time()
+for i in range(10):
+	df2 = df.copy()
+	df2.sort_values('average', ascending=False)
+elapsed_time = time.time() - start
+print ("ソート10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
+df = df2.copy()
+
+# 絞り込み
+start = time.time()
+for i in range(10):
+	df2 = df[50 <= df['average']]
+elapsed_time = time.time() - start
+print ("絞り込み10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
+
+# 平均 iat
+start = time.time()
+for i in range(10):
+	for idx in range(len(df)):
+		df.iat[idx, 6] = str((float(df.iat[idx, 1]) + float(df.iat[idx, 2])
+			+ float(df.iat[idx, 3]) + float(df.iat[idx, 4]) + float(df.iat[idx, 5])/5.0))
+elapsed_time = time.time() - start
+print ("平均for10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
---
+++ use10000dataList.py
@@ -0,0 +1,46 @@
+﻿import csv
+import numpy as np
+import copy
+import time
+
+with open('10000data.csv') as f:
+	reader = csv.reader(f)
+	list = [row for row in reader]
+listHeader = list.pop(0)
+
+listHeader.extend(['average'])
+# 平均(計測用)
+start = time.time()
+for i in range(10):
+	list2 = copy.deepcopy(list)
+	for idx, row in enumerate(list2):
+		row.extend('0')
+		row[7] = str((float(row[2]) + float(row[3]) + float(row[4]) + float(row[5]) + float(row[6]))/5.0)
+elapsed_time = time.time() - start
+print ("平均処理10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
+
+# 平均
+for idx, row in enumerate(list):
+	row.extend('0')
+	row[7] = str((float(row[2]) + float(row[3]) + float(row[4]) + float(row[5]) + float(row[6]))/5.0)
+
+# ソート(計測用)
+start = time.time()
+for i in range(10):
+	list2 = sorted(list, key=lambda x: x[7], reverse=True)
+elapsed_time = time.time() - start
+print ("ソート10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
+
+# ソート
+for i in range(10):
+	list = sorted(list, key=lambda x: x[7], reverse=True)
+
+# 絞り込み(計測用)
+start = time.time()
+for i in range(10):
+	list2 = []
+	for idx, row in enumerate(list):
+		if 50 <= float(row[7]):
+			list2.append(row)
+elapsed_time = time.time() - start
+print ("絞り込み10回平均所要時間:{0}".format(elapsed_time/10) + "[sec]")
