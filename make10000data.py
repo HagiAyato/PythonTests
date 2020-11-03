c33141d184654dd4bc2dd7fc4d803be3bb7f75e2
DESKTOP-STRK401\hagirainbow <h8a9g11i@gmail.com>
2020-11-03 11:00:10 +0900

    テストデータ作成

---
+++ make10000data.py
@@ -0,0 +1,16 @@
+﻿import pandas as pd
+from numpy.random import *
+
+print("make10000data START")
+
+df = pd.DataFrame(columns=['studentNo', 'subjectA', 'subjectB', 'subjectC', 'subjectD', 'subjectE'])
+dfcol = df.columns
+
+for idx in range(10000):
+	studentNo = str(idx + 1)
+	df = df.append(pd.Series([studentNo, randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100)], index = dfcol, name = studentNo))
+	if(idx % 100 == 0):
+		print(str(idx) + "件目完了")
+df.to_csv('10000data.csv')
+
+print("make10000data END")
